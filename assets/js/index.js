d7fe1f494d5e1cbe2c9fa207a04bd3e533a6d4f2
ashma <ashma20570218@gmail.com>
2022-08-16 13:05:10 +0545

    css changed

--- assets/js/index.js
+++ assets/js/index.js
@@ -11,10 +11,10 @@ $ ( document ).ready(function(){
     // Contact box- button disable
     $('input[type="checkbox"]').click(function(){
         if($(this).prop("checked") == true){
-            $( '.btn' ).prop('disabled', false);
+            $( '.disbtn' ).prop('disabled', false);
         }
         else if($(this).prop("checked") == false){
-            $( '.btn' ).prop('disabled', true);
+            $( '.disbtn' ).prop('disabled', true);
         }
     });
 
@@ -60,9 +60,41 @@ $ ( document ).ready(function(){
               slidesToScroll: 1
             }
           }
-          // You can unslick at a given breakpoint now by adding:
-          // settings: "unslick"
-          // instead of a settings object
         ]
-      });
+    });
+
+    // idea-solution slider
+    $('.idea-solutions-wrapper').slick({
+      dots: false,
+      infinite: true,
+      arrows: false,
+      speed: 300,
+      slidesToShow: 5,
+      slidesToScroll: 5,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: false
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
+  });
 })
