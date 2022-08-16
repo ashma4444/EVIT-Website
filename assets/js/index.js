b5efa6a6fe968898e3aeb7060fa92ec3abdeacc8
ashma <ashma20570218@gmail.com>
2022-08-16 12:11:19 +0545

    projects icon slider

--- assets/js/index.js
+++ assets/js/index.js
@@ -18,7 +18,7 @@ $ ( document ).ready(function(){
         }
     });
 
-    // slider
+    // quotes slider
     $('.quote-container-wrapper').slick({
         dots:true,
         arrows:false,
@@ -26,5 +26,43 @@ $ ( document ).ready(function(){
         autoplaySpeed:1500,
         fade:true,
         fadeSpeed: 1000
-  });
+    });
+
+    // projects icon slider
+    $('.project-icons-wrapper').slick({
+        dots: false,
+        infinite: false,
+        arrows: false,
+        speed: 300,
+        slidesToShow: 5,
+        slidesToScroll: 5,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3,
+              infinite: false,
+              dots: false
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+          // You can unslick at a given breakpoint now by adding:
+          // settings: "unslick"
+          // instead of a settings object
+        ]
+      });
 })
